98eff626c5f47ee28373df4767025b75afa76129
Ali Abbasi <aliabbasivadeghani100@gmail.com>
2021-04-19 22:48:58 +0430

    commit1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationManger applicationManger = new ApplicationManger();
         applicationManger.run();
+        System.out.println("hello");
     }
 }
