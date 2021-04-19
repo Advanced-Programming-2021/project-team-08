acb61226d0a477f2279f76bbe39a3c77c3782bf9
mohsen5980 <mohsen59shirazi80@gmail.com>
2021-04-19 22:57:55 +0330

    changed hello to changed it again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationManger applicationManger = new ApplicationManger();
         applicationManger.run();
-        System.out.println("hello");
+        System.out.println("I changed it again");
     }
 }
