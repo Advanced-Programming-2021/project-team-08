9d4725dce9397253c70fe83ac9fe15188cda427f
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2021-04-19 22:20:44 +0430

    -test commit

--- src/main/java/Menus/RegisterScene.java
+++ src/main/java/Menus/RegisterScene.java
@@ -4,7 +4,8 @@ public class RegisterScene extends Scene {
     @Override
     protected int getUserCommand() {
         String userInput = scanner.nextLine().trim();
-
+        System.out.println("test commit!");
+        
         return 1;
     }
 }
