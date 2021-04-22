dc6088089430c47ff9fedb2639401cb79477fc82
Abolfazl Kashi <abolfazlkashi81@gmail.com>
2021-04-22 17:15:27 +0430

    - fix a warning

--- src/main/java/Controllers/RegisterController.java
+++ src/main/java/Controllers/RegisterController.java
@@ -9,41 +9,48 @@ import Views.Menus.SceneName;
 import java.util.HashMap;
 
 public class RegisterController {
-    public static int registerUser(String userInput){
-        HashMap<String , CommandFieldType> fieldsOfRegisterUser=new HashMap<>();
-        fieldsOfRegisterUser.put("username",CommandFieldType.STRING);
-        fieldsOfRegisterUser.put("password",CommandFieldType.STRING);
-        fieldsOfRegisterUser.put("nickname",CommandFieldType.STRING);
-//        Command registerCommand = Command.parseCommand(userInput,fieldsOfRegisterUser);
-//        if (User.doesUsernameExists(registerCommand.getField("username")))
-//            System.out.printf("user with username" + registerCommand.getField("username") + "already exists");
-//        else if (User.doesUsernameExists(registerCommand.getField("nickname")))
-//            System.out.printf("user with nickname <nickname> already exists");
-//        else {
-//            new User(registerCommand.getField("username"),registerCommand.getField("nickname"),registerCommand.getField("password"));
-//            System.out.printf("user created successfully!");
-//        }
+    public static int registerUser(String userInput) {
+        HashMap<String, CommandFieldType> fieldsOfRegisterUser = new HashMap<>();
+        fieldsOfRegisterUser.put("username", CommandFieldType.STRING);
+        fieldsOfRegisterUser.put("password", CommandFieldType.STRING);
+        fieldsOfRegisterUser.put("nickname", CommandFieldType.STRING);
+        try {
+            Command registerCommand = Command.parseCommand(userInput, fieldsOfRegisterUser);
+            if (User.doesUsernameExists(registerCommand.getField("username")))
+                System.out.printf("user with username" + registerCommand.getField("username") + "already exists");
+            else if (User.doesUsernameExists(registerCommand.getField("nickname")))
+                System.out.printf("user with nickname <nickname> already exists");
+            else {
+                new User(registerCommand.getField("username"), registerCommand.getField("nickname"), registerCommand.getField("password"));
+                System.out.printf("user created successfully!");
+            }
+        } catch (Views.Menus.Exceptions.ParseCommandException e) {
+            System.out.println("Invalid command");
+        }
         return 1;
     }
 
-    public static int loginUser(String userInput){
-        HashMap<String , CommandFieldType> fieldsOfLoginUser=new HashMap<>();
-        fieldsOfLoginUser.put("username",CommandFieldType.STRING);
-        fieldsOfLoginUser.put("nickname",CommandFieldType.STRING);
-//        Command loginCommand = Command.parseCommand(userInput,fieldsOfLoginUser);
-//        if (!User.doesUsernameExists(loginCommand.getField("username")))
-//            System.out.printf("Username and password didn’t match!");
-//        else {
-//            if (User.loginUser(loginCommand.getField("username"),loginCommand.getField("password"))){
-//                System.out.printf("user logged in successfully!");
-//                ApplicationManger.goToScene(SceneName.MAIN_MENU);
-//                return 0;
-//            }
-//            else {
-//                System.out.printf("Username and password didn’t match!");
-//            }
+    public static int loginUser(String userInput) {
+        HashMap<String, CommandFieldType> fieldsOfLoginUser = new HashMap<>();
+        fieldsOfLoginUser.put("username", CommandFieldType.STRING);
+        fieldsOfLoginUser.put("nickname", CommandFieldType.STRING);
+        try {
+            Command loginCommand = Command.parseCommand(userInput, fieldsOfLoginUser);
+            if (!User.doesUsernameExists(loginCommand.getField("username")))
+                System.out.printf("Username and password didn't match!");
+            else {
+                if (User.loginUser(loginCommand.getField("username"), loginCommand.getField("password"))) {
+                    System.out.printf("user logged in successfully!");
+                    ApplicationManger.goToScene(SceneName.MAIN_MENU);
+                    return 0;
+                } else {
+                    System.out.printf("Username and password didn't match!");
+                }
 
-//        }
+            }
+        } catch (Views.Menus.Exceptions.ParseCommandException e) {
+            System.out.println("Invalid command");
+        }
         return 1;
     }
 }
