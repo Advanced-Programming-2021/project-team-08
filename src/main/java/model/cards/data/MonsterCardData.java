3edb44266a5b141ad2bc28b6bc7e6bbacbc837b3
mohsen5980 <mohsen59shirazi80@gmail.com>
2021-05-14 01:07:43 +0330

    MonsterCardData updated MonsterAttribute updated Class ReadMonsterCardsData added

--- src/main/java/model/cards/data/MonsterCardData.java
+++ src/main/java/model/cards/data/MonsterCardData.java
@@ -2,14 +2,112 @@ package model.cards.data;
 
 import model.enums.MonsterAttribute;
 
+import java.util.ArrayList;
+
 public class MonsterCardData extends CardData{
 
     private int level;
+    private String name;
     private MonsterAttribute attribute;
     private String type;
+    private String monsterType;
 
     private int attackPoints, defencePoints;
+    int price;
     private String cardDescription;
 
+    private static ArrayList<MonsterCardData> allMonsterCardData = new ArrayList<>();
 
+    public MonsterCardData() {
+        allMonsterCardData.add(this);
+    }
+
+    public void setAttackPoints(int attackPoints) {
+        this.attackPoints = attackPoints;
+    }
+
+    public void setAttribute(MonsterAttribute attribute) {
+        this.attribute = attribute;
+    }
+
+    public void setCardDescription(String cardDescription) {
+        this.cardDescription = cardDescription;
+    }
+
+    public void setDefencePoints(int defencePoints) {
+        this.defencePoints = defencePoints;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMonsterType(String monsterType) {
+        this.monsterType = monsterType;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public MonsterAttribute getAttribute() {
+        return attribute;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getAttackPoints() {
+        return attackPoints;
+    }
+
+    public String getMonsterType() {
+        return monsterType;
+    }
+
+    public int getDefencePoints() {
+        return defencePoints;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getCardDescription() {
+        return cardDescription;
+    }
+
+    public static void printAllCard() {
+        for(MonsterCardData monsterCardData: allMonsterCardData) {
+            try{
+                System.out.println("name: " + monsterCardData.getName() + "  level: " + monsterCardData.getLevel());
+                System.out.println("monster attribute: " + monsterCardData.getAttribute());
+                System.out.println("monster type: " + monsterCardData.getMonsterType());
+                System.out.println("monster card type:" + monsterCardData.getType());
+                System.out.println("monster attack: " + monsterCardData.getAttackPoints() + "  monster defence point: " + monsterCardData.getDefencePoints());
+                System.out.println(monsterCardData.getCardDescription());
+                System.out.println("monster price: " + monsterCardData.getPrice());
+                System.out.println();
+            }catch (NullPointerException e) {
+                System.out.println("null pointer in printing cards");
+            }
+        }
+    }
 }
---
+++ src/main/java/model/cards/data/ReadMonsterCardsData.java
@@ -0,0 +1,118 @@
+package model.cards.data;
+
+import model.enums.MonsterAttribute;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
+public class ReadMonsterCardsData {
+
+    public void readCardsData() {
+        try {
+            BufferedReader csvReader = new BufferedReader(new FileReader("Monster.csv"));
+            String row;
+            int rowNum = 0;
+            row = csvReader.readLine();
+            while ((row = csvReader.readLine()) != null) {
+                String[] data = row.split(",");
+                readACardData(data);
+            }
+            csvReader.close();
+        }catch (Exception e) {
+            System.out.println("some thing was wrong");
+        }
+    }
+
+    private void readACardData(String[] rowData) {
+        MonsterCardData monsterCardData = new MonsterCardData();
+        setMonsterName(rowData[0].trim(), monsterCardData);
+        setMonsterLevel(rowData[1].trim(), monsterCardData);
+        setMonsterAttribute(rowData[2].trim(), monsterCardData);
+        setMonsterType(rowData[3].trim(), monsterCardData);
+        setMonsterCardType(rowData[4].trim(), monsterCardData);
+        setMonsterAttack(rowData[5].trim(), monsterCardData);
+        setMonsterDefence(rowData[6].trim(), monsterCardData);
+        setMonsterDescription(rowData[7].trim(), monsterCardData);
+        setMonsterPrice(rowData[8].trim(), monsterCardData);
+    }
+
+    private void setMonsterName(String name, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setName(name);
+        }catch (Exception NullPointerException) {
+            System.out.println("name didn't found");
+        }
+    }
+
+    private void setMonsterLevel(String level, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setLevel(Integer.parseInt(level));
+        }catch (Exception e) {
+            System.out.println("level wasn't integer");
+        }
+    }
+
+    private void setMonsterAttribute(String attribute, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setAttribute(MonsterAttribute.valueOf(attribute));
+        }catch (Exception e) {
+            System.out.println("attribute enum failed");
+        }
+    }
+
+    private void setMonsterType(String monsterType, MonsterCardData monsterCardData) {
+        try{
+            monsterCardData.setMonsterType(monsterType);
+        }catch (Exception e) {
+            System.out.println("set monster type was wrong");
+        }
+    }
+
+    private void setMonsterCardType(String cardType, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setType(cardType);
+        }
+        catch (Exception e) {
+            System.out.println("set type was wrong");
+        }
+    }
+
+    private void setMonsterAttack(String monsterAttack, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setAttackPoints(Integer.parseInt(monsterAttack));
+        }catch (Exception e) {
+            System.out.println("set attack point failed");
+        }
+    }
+
+    private void setMonsterDefence(String defence, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setDefencePoints(Integer.parseInt(defence));
+        }catch (Exception e) {
+            System.out.println("set defence point failed");
+        }
+    }
+
+    private void setMonsterDescription(String description, MonsterCardData monsterCardData) {
+        try {
+            description = description.replace("\"", "");
+            monsterCardData.setCardDescription(description);
+        }catch (Exception e) {
+            System.out.println("set description failed");
+        }
+    }
+
+    private void setMonsterPrice(String price, MonsterCardData monsterCardData) {
+        try {
+            monsterCardData.setPrice(Integer.parseInt(price));
+        }
+        catch (Exception e) {
+            System.out.println("failed price: " + price);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReadMonsterCardsData().readCardsData();
+        MonsterCardData.printAllCard();
+    }
+}
