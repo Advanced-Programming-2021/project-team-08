ea422502ab8f09e0fe81d258740ff5571e9c3b5e
Ali Abbasi <aliabbasivadeghani100@gmail.com>
2021-07-14 01:27:51 +0430

    scoreboard

--- Client/src/main/java/view/menus/ScoreboardMenu.java
+++ Client/src/main/java/view/menus/ScoreboardMenu.java
@@ -3,15 +3,20 @@ package view.menus;
 import controller.ApplicationManger;
 import controller.User;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import model.UserData;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -20,35 +25,50 @@ import java.util.regex.Pattern;
 
 public class ScoreboardMenu extends Scene {
     public VBox vBox;
-    public HBox hBox;
+
 
     @FXML
-    void initialize(){
+    void initialize() {
         int i;
-        Label rank=new Label();
-        Label nickName=new Label();
-        Label point=new Label();
         UserData[] scoreboard = User.getAllUserData();
         Arrays.sort(scoreboard, new sort());
-        int size=Math.min(20,scoreboard.length);
-        UserData[] topPlayers=new UserData[20];
-        for (i=0;i<size;i++)
-            topPlayers[i]=scoreboard[i];
-        int[] renks=getRank(topPlayers,size);
-        for (i=0;i<size;i++) {
+        int size = Math.min(20, scoreboard.length);
+        UserData[] topPlayers = new UserData[size];
+        for (i = 0; i < size; i++)
+            topPlayers[i] = scoreboard[i];
+        int[] ranks = getRank(topPlayers, size);
+        for (i = 0; i < size; i++) {
+            Label rank = new Label();
+            Label nickName = new Label();
+            Label point = new Label();
+            HBox hBox = new HBox();
+            hBox.setSpacing(20);
+            rank.setAlignment(Pos.CENTER);
+            nickName.setAlignment(Pos.CENTER);
+            point.setAlignment(Pos.CENTER);
+            rank.setStyle("-fx-background-color: rgb(182, 202, 242)");
+            nickName.setStyle("-fx-background-color: rgb(182, 202, 242)");
+            point.setStyle("-fx-background-color: rgb(182, 202, 242)");
+            rank.setPrefWidth(140);
+            nickName.setPrefWidth(200);
+            point.setPrefWidth(50);
             nickName.setText(topPlayers[i].getNickname());
             point.setText(String.valueOf(topPlayers[i].getPoint()));
-            rank.setText(String.valueOf(renks[i]));
-            if (topPlayers[i].getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
+            rank.setText(String.valueOf(ranks[i]));
+            if (topPlayers[i].getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())) {
                 nickName.setTextFill(Color.GREEN);
                 point.setTextFill(Color.GREEN);
+                rank.setTextFill(Color.GREEN);
+                rank.setStyle("-fx-background-color: rgb(245, 166, 102)");
+                nickName.setStyle("-fx-background-color: rgb(245, 166, 102)");
+                point.setStyle("-fx-background-color: rgb(245, 166, 102)");
             }
             hBox.getChildren().clear();
-            vBox.getChildren().clear();
-            hBox.getChildren().add(0,rank);
-            hBox.getChildren().add(1,nickName);
-            hBox.getChildren().add(2,point);
-            vBox.getChildren().add(i,hBox);
+//            vBox.getChildren().clear();
+            hBox.getChildren().add(0, rank);
+            hBox.getChildren().add(1, nickName);
+            hBox.getChildren().add(2, point);
+            vBox.getChildren().add(hBox);
         }
     }
 
@@ -101,20 +121,25 @@ public class ScoreboardMenu extends Scene {
 
 
     public void back(MouseEvent mouseEvent) {
-        ApplicationManger.goToScene1(SceneName.PROFILE_MENU,false);
+        ApplicationManger.goToScene1(SceneName.PROFILE_MENU, false);
     }
 
-    public int[] getRank(UserData[] topPlayers,int size){
+    public int[] getRank(UserData[] topPlayers, int size) {
+        int counterOfSamePoint = 1;
         int pointOfPreviousUser = 0;
-        int[] rank=new int[size];
+        int[] rank = new int[size];
         int i;
-        for (i=0;i<size;i++) rank[i]=i;
-        for (i=0;i<size;i++){
-            if (topPlayers[i].getPoint()==pointOfPreviousUser)
-                rank[i]--;
-            pointOfPreviousUser=topPlayers[i].getPoint();
+        for (i = 0; i < size; i++) rank[i] = i;
+        for (i = 0; i < size; i++) {
+            if (topPlayers[i].getPoint() == pointOfPreviousUser) {
+                rank[i] -= counterOfSamePoint;
+                counterOfSamePoint++;
+            } else {
+                counterOfSamePoint = 1;
+            }
+            pointOfPreviousUser = topPlayers[i].getPoint();
         }
-        for (i=0;i<size;i++) rank[i]++;
+        for (i = 0; i < size; i++) rank[i]++;
         return rank;
     }
 }
