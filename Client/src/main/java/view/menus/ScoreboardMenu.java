c036c6d5de5cef3c869be5be588ecf5d08774aca
Ali Abbasi <aliabbasivadeghani100@gmail.com>
2021-07-13 16:09:47 +0430

    scoreboard

--- Client/src/main/java/view/menus/ScoreboardMenu.java
+++ Client/src/main/java/view/menus/ScoreboardMenu.java
@@ -4,12 +4,15 @@ import controller.ApplicationManger;
 import controller.User;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import model.UserData;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -21,58 +24,31 @@ public class ScoreboardMenu extends Scene {
 
     @FXML
     void initialize(){
+        int i;
         Label rank=new Label();
         Label nickName=new Label();
         Label point=new Label();
         UserData[] scoreboard = User.getAllUserData();
         Arrays.sort(scoreboard, new sort());
-        int rowNumber = 1;
-        int counterOfSamePoint = 0;
-        int pointOfPreviousUser = 0;
-        for (UserData userData : scoreboard) {
-
-            nickName.setText(userData.getNickname());
-            point.setText(String.valueOf(userData.getPoint()));
-            if (userData.getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
+        int size=Math.min(20,scoreboard.length);
+        UserData[] topPlayers=new UserData[20];
+        for (i=0;i<size;i++)
+            topPlayers[i]=scoreboard[i];
+        int[] renks=getRank(topPlayers,size);
+        for (i=0;i<size;i++) {
+            nickName.setText(topPlayers[i].getNickname());
+            point.setText(String.valueOf(topPlayers[i].getPoint()));
+            rank.setText(String.valueOf(renks[i]));
+            if (topPlayers[i].getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
                 nickName.setTextFill(Color.GREEN);
                 point.setTextFill(Color.GREEN);
             }
+            hBox.getChildren().clear();
+            vBox.getChildren().clear();
+            hBox.getChildren().add(0,rank);
             hBox.getChildren().add(1,nickName);
             hBox.getChildren().add(2,point);
-
-            if (rowNumber == 1) {
-                rank.setText(String.valueOf(rowNumber));
-                if (userData.getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
-                    rank.setTextFill(Color.GREEN);
-                }
-                hBox.getChildren().add(0,rank);
-                System.out.println(rowNumber + "- " + userData.getNickname() + ": " + userData.getPoint());
-                rowNumber++;
-            } else {
-                if (pointOfPreviousUser == userData.getPoint()) {
-                    rank.setText(String.valueOf(rowNumber-1));
-                    if (userData.getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
-                        rank.setTextFill(Color.GREEN);
-                    }
-                    hBox.getChildren().add(0,rank);
-                    System.out.println((rowNumber - 1) + "- " + userData.getNickname() + ": " + userData.getPoint());
-                    counterOfSamePoint++;
-                } else {
-                    if (counterOfSamePoint != 0) {
-                        rowNumber += counterOfSamePoint;
-                        counterOfSamePoint = 0;
-                    }
-                    rank.setText(String.valueOf(rowNumber));
-                    if (userData.getUsername().equals(ApplicationManger.getLoggedInUser().getUsername())){
-                        rank.setTextFill(Color.GREEN);
-                    }
-                    hBox.getChildren().add(0,rank);
-                    System.out.println(rowNumber + "- " + userData.getNickname() + ": " + userData.getPoint());
-                    rowNumber++;
-                }
-            }
-            vBox.getChildren().add(rowNumber-1,hBox);
-            pointOfPreviousUser = userData.getPoint();
+            vBox.getChildren().add(i,hBox);
         }
     }
 
@@ -126,6 +102,20 @@ public class ScoreboardMenu extends Scene {
 
     public void back(MouseEvent mouseEvent) {
         ApplicationManger.goToScene1(SceneName.PROFILE_MENU,false);
+    }
+
+    public int[] getRank(UserData[] topPlayers,int size){
+        int pointOfPreviousUser = 0;
+        int[] rank=new int[size];
+        int i;
+        for (i=0;i<size;i++) rank[i]=i;
+        for (i=0;i<size;i++){
+            if (topPlayers[i].getPoint()==pointOfPreviousUser)
+                rank[i]--;
+            pointOfPreviousUser=topPlayers[i].getPoint();
+        }
+        for (i=0;i<size;i++) rank[i]++;
+        return rank;
     }
 }
 
